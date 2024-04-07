92ec6c85602efda10dd8ed732dcdb7ef522cb8fd
muhohoweb <muhohokenya@users.noreply.github.com>
2024-04-07 15:02:20 +0300

    Initial commit

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TodoApi.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    public class TodoContext:DbContext
+	{
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
+
---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TodoApi.Models
+{
+	public class TodoItem
+	{
+		public TodoItem(){
+            // Constructor
+        }
+
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
+
