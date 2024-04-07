93463b67bbd494b4f0eb589b2bdeee96836686cd
muhohoweb <muhohokenya@users.noreply.github.com>
2024-04-07 15:42:32 +0300

    Enable Swaggwer in prod

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
