674a298a6f67571c86a77ca0b14582fcafdfbbbf
muhohoweb <muhohokenya@users.noreply.github.com>
2024-04-11 19:15:32 +0300

    Allow Cors

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -2,11 +2,30 @@
 using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var allowOriginsPolicy = "origin";
 // Add services to the container.
 
-builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowOriginsPolicy, b =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            b.AllowAnyOrigin();
+        }
+
+        b.AllowAnyMethod()
+            .AllowAnyHeader()
+            .SetIsOriginAllowed((_) => true);
+    });
+});
+
+
+
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
@@ -15,14 +34,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(allowOriginsPolicy);
 
 app.UseHttpsRedirection();
 
